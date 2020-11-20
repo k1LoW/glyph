9fd8f6f991de6e3a4d2bcca0cce54d21c7ca9243
k1LoW <k1lowxb@gmail.com>
2020-11-21 01:35:43 +0900

    Fix

--- example/icons/database.go
+++ example/icons/database.go
@@ -9,11 +9,11 @@ import (
 
 func main() {
 	g := glyph.New()
-	g.Draw(strings.Split("b0 d0 h0 j0 h2 d2 b0", " "))
-	g.Draw(strings.Split("b0 b6 d8 h8 j6 j0", " "))
-	g.Draw(strings.Split("b0 d2 h2 j0", " "))
-	g.Draw(strings.Split("b2 d4 h4 j2", " "))
-	g.Draw(strings.Split("b4 d6 h6 j4", " "))
+	g.AddLine(strings.Split("b0 d0 h0 j0 h2 d2 b0", " "))
+	g.AddLine(strings.Split("b0 b6 d8 h8 j6 j0", " "))
+	g.AddLine(strings.Split("b0 d2 h2 j0", " "))
+	g.AddLine(strings.Split("b2 d4 h4 j2", " "))
+	g.AddLine(strings.Split("b4 d6 h6 j4", " "))
 	if err := g.Write(os.Stdout); err != nil {
 		panic(err)
 	}
--- glyph.go
+++ glyph.go
@@ -1,16 +1,24 @@
 package glyph
 
 import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
 	"io"
 	"strings"
 
 	svgo "github.com/ajstarks/svgo/float"
+	"github.com/elliotchance/orderedmap"
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
 )
 
-var defaultOpt = []string{"stroke:#4B75B9", "stroke-width:4", "stroke-linecap:round", "stroke-linejoin:round"}
+var defaultLineOpt = []string{"stroke:#4B75B9", "stroke-width:4", "stroke-linecap:round", "stroke-linejoin:round"}
 
 type Line struct {
 	points []*Point
+	opts   *orderedmap.OrderedMap
 }
 
 type Glyph struct {
@@ -29,7 +37,12 @@ func New() *Glyph {
 	}
 }
 
-func (g *Glyph) Draw(strp []string) error {
+func (g *Glyph) AddLine(strp []string, opts ...string) error {
+	m := orderedmap.NewOrderedMap()
+	for _, opt := range defaultLineOpt {
+		splited := strings.Split(opt, ":")
+		m.Set(strings.Trim(splited[0], " ;"), strings.Trim(splited[1], " ;"))
+	}
 	l := &Line{}
 	ps := points()
 	for _, k := range strp {
@@ -39,11 +52,32 @@ func (g *Glyph) Draw(strp []string) error {
 		}
 		l.points = append(l.points, p)
 	}
+	if l.points[0].X == l.points[len(l.points)-1].X && l.points[0].Y == l.points[len(l.points)-1].Y {
+		// polygon
+		m.Set("fill", "#FFFFFF")
+	} else {
+		// polyline
+		m.Set("fill-opacity", "0.0")
+	}
+	for _, opt := range opts {
+		splited := strings.Split(opt, ":")
+		m.Set(strings.Trim(splited[0], " ;"), strings.Trim(splited[1], " ;"))
+	}
+	l.opts = m
+
 	g.lines = append(g.lines, l)
 	return nil
 }
 
 func (g *Glyph) Write(w io.Writer) error {
+	return g.writeSVG(w)
+}
+
+func (g *Glyph) WriteImage(w io.Writer) error {
+	return g.writePNG(w)
+}
+
+func (g *Glyph) writeSVG(w io.Writer) error {
 	svg := svgo.New(w)
 	svg.Startview(g.w, g.h, g.minx, g.miny, g.vw, g.vh)
 	for _, l := range g.lines {
@@ -53,16 +87,41 @@ func (g *Glyph) Write(w io.Writer) error {
 			x = append(x, p.X)
 			y = append(y, p.Y)
 		}
+		opts := []string{}
+		for _, k := range l.opts.Keys() {
+			v, _ := l.opts.Get(k)
+			opts = append(opts, fmt.Sprintf("%s:%s", k, v.(string)))
+		}
 		if l.points[0].X == l.points[len(l.points)-1].X && l.points[0].Y == l.points[len(l.points)-1].Y {
 			// polygon
-			opt := append(defaultOpt, "fill:#FFFFFF")
-			svg.Polygon(x, y, strings.Join(opt, "; "))
+			svg.Polygon(x, y, strings.Join(opts, "; "))
 		} else {
 			// polyline
-			opt := append(defaultOpt, "fill-opacity:0.0")
-			svg.Polyline(x, y, strings.Join(opt, "; "))
+			svg.Polyline(x, y, strings.Join(opts, "; "))
 		}
 	}
 	svg.End()
 	return nil
+}
+
+func (g *Glyph) writePNG(w io.Writer) error {
+	svgbuf := new(bytes.Buffer)
+	if err := g.Write(svgbuf); err != nil {
+		return err
+	}
+	icon, err := oksvg.ReadIconStream(svgbuf)
+	if err != nil {
+		return err
+	}
+	icon.SetTarget(0, 0, g.w, g.h)
+	rgba := image.NewRGBA(image.Rect(0, 0, round(g.w), round(g.h)))
+	icon.Draw(rasterx.NewDasher(round(g.w), round(g.h), rasterx.NewScannerGV(round(g.w), round(g.h), rgba, rgba.Bounds())), 1)
+	return png.Encode(w, rgba)
+}
+
+func round(v float64) int {
+	if v < 0 {
+		return int(v - 0.5)
+	}
+	return int(v + 0.5)
 }
